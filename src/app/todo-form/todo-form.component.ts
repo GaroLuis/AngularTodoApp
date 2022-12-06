9ba757778aca445a4662add4e872a0230d6ed9eb
GaroLuis <garoe@aircury.com>
2022-12-06 14:22:49 +0100

    Added simple todo form

---
+++ src/app/todo-form/todo-form.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from "@angular/forms";
+
+@Component({
+  selector: 'todo-form',
+  templateUrl: './todo-form.component.html',
+  styleUrls: ['./todo-form.component.sass']
+})
+export class TodoFormComponent {
+  todoForm = this.formBuilder.group({
+    todo: ['', Validators.compose([Validators.maxLength(10), Validators.required])]
+  })
+
+  onSubmit() {
+    console.log(this.todoForm.value);
+  }
+
+  constructor(private formBuilder: FormBuilder) {
+  }
+}
